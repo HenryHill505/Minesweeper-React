5432f6d747f33f2ac34e5a21e4c721aaff087c23
HenryHill505 <39996980+HenryHill505@users.noreply.github.com>
2018-11-08 14:21:12 -0600

    In <Board> wrote placeFlag(), removeFlag(), & added flagCount to state

--- src/Board.js
+++ src/Board.js
@@ -1,15 +1,33 @@
 import React from "react";
 import Field from "./Field.js";
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flagCount: this.props.mineCount
+    };
+  }
+
+  placeFlag() {
+    let newCount = this.state.flagCount - 1;
+    this.setState({ flagCount: newCount });
+  }
+
+  removeFlag() {
+    let newCount = this.state.flagCount + 1;
+    this.setState({ flagCount: newCount });
+  }
   render() {
     return (
-      <Field
-        rowCount={this.props.rowCount}
-        columnCount={this.props.columnCount}
-        mineCount={this.props.mineCount}
-        loseFunction={this.props.loseFunction}
-        winFunction={this.props.winFunction}
-      />
+      <div>
+        <Field
+          rowCount={this.props.rowCount}
+          columnCount={this.props.columnCount}
+          mineCount={this.props.mineCount}
+          loseFunction={this.props.loseFunction}
+          winFunction={this.props.winFunction}
+        />
+      </div>
     );
   }
 }
